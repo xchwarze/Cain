5347d153e1b2fe7d97d8fcd0227c55fe58f6d8b9
DSR! <xchwarze@gmail.com>
2023-01-04 12:51:20 -0300

    service update 2023

--- Tools/oui-to-cain.php
+++ Tools/oui-to-cain.php
@@ -1,37 +1,43 @@
 <?php
+
 //download url:
-// http://standards-oui.ieee.org/oui/oui.txt
-// https://github.com/vcrhonek/hwdata/blob/RHEL7/oui.txt
+// https://standards-oui.ieee.org/oui/oui.txt
 // https://github.com/vcrhonek/hwdata/blob/master/oui.txt
+$oui_download_url = 'https://standards-oui.ieee.org/oui/oui.txt';
 
-// Config
-$oui_file = dirname(__FILE__) . DIRECTORY_SEPARATOR . "oui.txt.original";
-$oui_download_url = "http://standards-oui.ieee.org/oui/oui.txt";
-
-
-
-// Script...
-if (!empty($oui_download_url)) {
-	echo "[+] Downloading updated oui.txt...\n";
-	$remote_file = file_get_contents($oui_download_url);
-	file_put_contents("oui.txt.original", $remote_file);
-	echo "[+] Download complete!\n";
+if (!file_exists('oui.original')) {
+    echo "[+] Downloading updated oui.txt...\n";
+    $remote_file = file_get_contents($oui_download_url);
+    file_put_contents('oui.original', $remote_file);
 }
 
 
 echo "[+] Processing oui.txt...\n";
-$total = 0;
-$output = [];
+@unlink('oui.txt');
 
-foreach (file($oui_file) as $line) {
-	if (strpos($line, "(base 16)") !== false){
-		$total++;
-		$output[] = $line;
-	}
+$flag   = '(base 16)';
+$total  = 0;
+$output = [];
+$index  = [];
+foreach (file('oui.original') as $line) {
+    if (strpos($line, $flag) !== false){
+        $parts = explode($flag, $line);
+        $id    = mb_strtoupper(trim($parts[0]));
+        if (!in_array($id, $index, true)) {
+            $total++;
+            $index[] = $id;
+            $output[] = $line;
+            //$output[] = str_replace(['.', ','], '', $line);
+        }
+    }
 }
 
 sort($output);
-file_put_contents("oui.txt", implode("", $output));
+$output[] = '';
+
+file_put_contents('oui.txt', implode("\n", $output));
+unlink('oui.original');
+
 echo "[+] Processing complete!\n";
-echo "[*] File: oui.txt Lines: {$total}\n";
+echo "[*] File: oui.txt - Lines: {$total}\n";
 echo "[!] Replace Cain original oui.txt with new file!\n";
